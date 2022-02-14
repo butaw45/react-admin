86b171435ca362b8f4e4855b6b17e1118a7c1271
Muhammad Haris <kumantheas@gmail.com>
2022-02-14 13:30:49 +0700

    Login

--- src/App.tsx
+++ src/App.tsx
@@ -7,6 +7,7 @@ import './App.css';
 import Dashboard from './pages/Dashboard';
 import Users from './pages/Users';
 import Register from './pages/Register';
+import Login from './pages/Login';
 
 function App() {
   return (
@@ -16,6 +17,7 @@ function App() {
           <Route path={"/"} element={<Dashboard />} />
           <Route path={"/users"} element={<Users />} />
           <Route path={"/register"} element={<Register />} />
+          <Route path={"/login"} element={<Login />} />
       </Routes>
       
       </BrowserRouter>
