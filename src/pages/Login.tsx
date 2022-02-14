1809459d0676ae1c92924a823081992e579166cf
Muhammad Haris <kumantheas@gmail.com>
2022-02-14 14:04:34 +0700

    Login

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,10 +1,41 @@
-import React from 'react';
+import React, { SyntheticEvent, useState } from 'react';
+import axios from 'axios';
+import { Navigate } from 'react-router-dom';
 
 const Login = () => {
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+    const [navigate, setNavigate] = useState(false);
+
+    const submit = async (e: SyntheticEvent) =>{
+        e.preventDefault();
+
+        await axios.post('http://localhost:8000/api/login', {
+            email,
+            password
+        }, {withCredentials: true});
+    
+        setNavigate(true);
+    }
+    if(navigate){
+    return <Navigate to={'/'} />;
+    }
+
     return (
-        <>
-            Login
-        </>
+
+        <main className="form-signin">
+        <form onSubmit={submit}>
+          <h1 className="h3 mb-3 fw-normal">Please sign in</h1>
+          
+          <input type="email" className="form-control" placeholder="Email" required onChange={e => setEmail(e.target.value)}/>
+          <input type="password" className="form-control" placeholder="Password" required onChange={e => setPassword(e.target.value)}/>
+         
+
+          <button className="w-100 btn btn-lg btn-primary" type="submit">
+            Submit
+          </button>
+        </form>
+      </main>
     );
 };
 
