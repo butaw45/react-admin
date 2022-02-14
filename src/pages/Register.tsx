10d985c87b1518bc942fbc019ef96390ca46cfb1
Muhammad Haris <kumantheas@gmail.com>
2022-02-14 13:11:54 +0700

    Axios Register

--- src/pages/Register.tsx
+++ src/pages/Register.tsx
@@ -1,5 +1,6 @@
 import React, { Component, SyntheticEvent } from 'react'
 import '../Login.css';
+import axios from 'axios';
 export default class Register extends Component {
   first_name = '';
   last_name = '';
@@ -7,15 +8,18 @@ export default class Register extends Component {
   password = '';
   password_confirm = '';
 
-  submit = (e: SyntheticEvent) => {
+  submit = async (e: SyntheticEvent) => {
     e.preventDefault();
-    console.log({
-        first_name: this.first_name,
-        last_name: this.last_name,
-        email: this.email,
-        password: this.password,
-        password_confirm: this.password_confirm,
+    
+   const response = await axios.post('http://localhost:8000/api/register', {
+      first_name: this.first_name,
+      last_name: this.last_name,
+      email: this.email,
+      password: this.password,
+      password_confirm: this.password_confirm
     });
+    
+    console.log(response);
     
   }
 
