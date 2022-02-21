a0226698f41d312bdbe0d12abc0dd55321a8f6be
Muhammad Haris <kumantheas@gmail.com>
2022-02-21 08:06:01 +0700

    Create Users

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,17 @@
+import './App.css';
+
 import React from 'react';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
+
+import Dashboard from './pages/Dashboard';
+import Login from './pages/Login';
+import Register from './pages/Register';
+import UserCreate from './pages/users/UserCreate';
+import Users from './pages/users/Users';
+
 // import logo from './logo.svg';
-import './App.css';
 // import Menu from './components/Menu';
 // import Nav from './components/Nav';
-import Dashboard from './pages/Dashboard';
-import Users from './pages/users/Users';
-import Register from './pages/Register';
-import Login from './pages/Login';
-
 function App() {
   return (
     <div className="App">
@@ -16,6 +19,7 @@ function App() {
       <Routes>
           <Route path={"/"} element={<Dashboard />} />
           <Route path={"/users"} element={<Users />} />
+          <Route path={"/users/create"} element={<UserCreate />} />
           <Route path={"/register"} element={<Register />} />
           <Route path={"/login"} element={<Login />} />
       </Routes>
--- src/pages/users/Users.tsx
+++ src/pages/users/Users.tsx
@@ -41,6 +41,10 @@ const Users = () => {
 
     return (
       <Wrapper>
+        <div className='pt-3 pb-2 mb-3 border-bottom'>
+          <Link to={"/users/create"} className='btn btn-sm btn-outline-secondary'>Add</Link>
+        </div>
+
         <div className="table-responsive">
         <table className="table table-striped table-sm">
           <thead>
